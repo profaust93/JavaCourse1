d0be3931b97ab6352c55fc113b6e03339c4353db
Ruslan <profaust93@gmail.com>
2016-10-19 20:27:51 +0300

    first task

---
+++ src/lv/javaguru/lessons/theme1/task0/SimpleIfStatementUsage.java
@@ -0,0 +1,30 @@
+package lv.javaguru.lessons.theme1.task0;
+
+import java.util.Scanner;
+
+public class SimpleIfStatementUsage {
+
+    public static void main(String[] args) {
+        int numberFromUser = getNumberFromUser();
+        printNumberSign(numberFromUser);
+    }
+
+    private static int getNumberFromUser() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number:");
+        int number = sc.nextInt();
+        return number;
+    }
+
+    private static void printNumberSign(int number) {
+        if (number == 0) {
+            System.out.println("This is zero");
+        } else if (number > 0) {
+            System.out.println("This is positive number");
+        } else {
+            System.out.println("This is negative number");
+        }
+    }
+
+
+}
